240d4f3eac63de25fa7927e270a446917058351d
C-L-South <codyli9219@gmail.com>
2026-06-12 11:25:40 -0500

    Update getExeCfg.js

--- getExeCfg.js
+++ getExeCfg.js
@@ -28,42 +28,42 @@ export function getExeCfg(caseId) {
         score_lim: [0.05, 0.1, 0.2],
 
         tau: 0.4,
-        highThresh: 10,
-        lowThresh: -10,
+        highThresh: 20,
+        lowThresh: -20,
 
-        minAmp: 15,
-        minPeriod: 0.4,
+        minAmp: 20,
+        minPeriod: 1,
         maxPeriod: 5.0,
         startTime: 1.0,
 
-        minPeriodWrn: 1,
-        maxPeriodWrn: 4
+        minPeriodWrn: 2,
+        maxPeriodWrn: 5
       };
 
     case 3: // lunge
       return {
         ang_idx: [13, 18],
         template: [135, 30, 40, 15].map(x => x * DTOR),
-        score_lim: [0.05, 0.1, 0.2],
+        score_lim: [0.15, 0.25, 0.4],
 
         tau: 0.4,
-        highThresh: 50,
-        lowThresh: -50,
+        highThresh: 55,
+        lowThresh: -40,
 
         minAmp: 50,
-        minPeriod: 0.4,
+        minPeriod: 0.5,
         maxPeriod: 5.0,
         startTime: 1.0,
 
-        minPeriodWrn: 1,
-        maxPeriodWrn: 4
+        minPeriodWrn: 2,
+        maxPeriodWrn: 5
       };
 
     case 4: // Child
       return {
         ang_idx: [8, 12],
         template: [150, 0, 30, 0].map(x => x * DTOR),
-        score_lim: [0.05, 0.1, 0.2],
+        score_lim: [0.1, 0.2, 0.3],
 
         tau: 0.4,
         highThresh: 5,
