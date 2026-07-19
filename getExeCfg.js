1392088cef71dca0b04a2cdce62673e27914c0a7
C-L-South <codyli9219@gmail.com>
2026-07-19 15:04:52 -0500

    Add configurations for new exercise cases

--- getExeCfg.js
+++ getExeCfg.js
@@ -81,6 +81,125 @@ export function getExeCfg(caseId) {
         minPeriodWrn: 1,
         maxPeriodWrn: 4
       };
+    case 5: // arm circle
+      return {
+        ang_idx: [7, 8],
+        template: [90, 20, 90, 20].map(x => x * DTOR),
+        score_lim: [0.15, 0.25, 0.35],
+        win_len: 2,
+
+        tau: 0.4,
+        highThresh: 10,
+        lowThresh: -10,
+
+        minAmp: 5.0,
+        minPeriod: 0.4,
+        maxPeriod: 5.0,
+        startTime: 1.0,
+
+        minPeriodWrn: 0.5,
+        maxPeriodWrn: 3
+      };
+
+    case 6: // butterfly stretch
+      return {
+        ang_idx: [12, 9],
+        template: [55, 0, 55, 0].map(x => x * DTOR),
+        score_lim: [0.1, 0.2, 0.3],
+        win_len: 0.2,
+
+        tau: 0.4,
+        highThresh: 5,
+        lowThresh: -2,
+
+        minAmp: 5.0,
+        minPeriod: 0.4,
+        maxPeriod: 5.0,
+        startTime: 1.0,
+
+        minPeriodWrn: 1,
+        maxPeriodWrn: 4
+      };
+
+    case 7: // cobra stretch
+      return {
+        ang_idx: [9, 12],
+        template: [130, 0, 130, 0].map(x => x * DTOR),
+        score_lim: [0.05, 0.1, 0.15],
+        win_len: 0.2,
+
+        tau: 0.4,
+        highThresh: 5,
+        lowThresh: -2,
+
+        minAmp: 5.0,
+        minPeriod: 0.4,
+        maxPeriod: 5.0,
+        startTime: 1.0,
+
+        minPeriodWrn: 1,
+        maxPeriodWrn: 4
+      };
+
+    case 8: // shoulder flexion raise
+      return {
+        ang_idx: [8, 7],
+        template: [90, 65, 90, 65].map(x => x * DTOR),
+        score_lim: [0.15, 0.25, 0.35],
+        win_len: 4,
+
+        tau: 0.4,
+        highThresh: 15,
+        lowThresh: -15,
+
+        minAmp: 5.0,
+        minPeriod: 0.4,
+        maxPeriod: 5.0,
+        startTime: 1.0,
+
+        minPeriodWrn: 1,
+        maxPeriodWrn: 4
+      };
+
+    case 9: // standing hamstring curl
+      return {
+        ang_idx: [13, 16],
+        template: [150, 45, 150, 45].map(x => x * DTOR),
+        score_lim: [0.15, 0.25, 0.35],
+        win_len: 3,
+
+        tau: 1.0,
+        highThresh: 5,
+        lowThresh: -2,
+
+        minAmp: 5.0,
+        minPeriod: 0.4,
+        maxPeriod: 5.0,
+        startTime: 1.0,
+
+        minPeriodWrn: 1,
+        maxPeriodWrn: 4
+      };
+
+    case 10: // standing march
+      return {
+        ang_idx: [16, 13],
+        template: [120, 50, 120, 50].map(x => x * DTOR),
+        score_lim: [0.15, 0.25, 0.35],
+        win_len: 2,
+
+        tau: 0.4,
+        highThresh: 10,
+        lowThresh: -5,
+
+        minAmp: 5.0,
+        minPeriod: 0.4,
+        maxPeriod: 5.0,
+        startTime: 1.0,
+
+        minPeriodWrn: 1,
+        maxPeriodWrn: 4
+      };
 
     default:
       throw new Error(`Unknown exercise case: ${caseId}`);
